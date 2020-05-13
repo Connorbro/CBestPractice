eebb23ae910174650e5b37040e313a8c66725ba1
connor <vbrookesy@hotmail.co.uk>
2020-05-13 11:06:12 +0100

    Cubed number function

--- Program.cs
+++ Program.cs
@@ -8,12 +8,18 @@ namespace BestPracticeC
         {
             HelloUser("Connor", 21);
             HelloUser("Leonie", 26);
+            cubedNumber(3);
 
            
         }
         static void HelloUser(string name, int age)
         {
             Console.WriteLine($"Hello {name} you are {age} years old");
+        }
+        static int cubedNumber(int num)
+        {
+            int result = num * num * num;
+            return result;
         }
     }
 }
