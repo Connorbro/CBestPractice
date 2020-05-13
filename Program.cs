f53e34bd258cd57004f41e47c0078ceb7a9abc45
connor <vbrookesy@hotmail.co.uk>
2020-05-13 12:41:42 +0100

    Upgraded the program so it can read more than 3 numbers and figure out the largest one

--- Program.cs
+++ Program.cs
@@ -6,20 +6,24 @@ namespace BestPracticeC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(GetLargerNumber(31, 85));
+            Console.WriteLine(GetLargerNumber(31, 85, 129));
 
 
         }
-        static int GetLargerNumber(int num1, int num2)
+        static int GetLargerNumber(int num1, int num2, int num3)
         {
             int result;
-            if (num1 > num2)
+            if (num1 >= num2 && num1 >= num3)
             {
                 result = num1;
             }
-            else
+            else if(num2 >= num1 && num2 >= num3)
             {
                 result = num2;
+            }
+            else
+            {
+                result = num3;
             }
 
             return result;
